ac4b9605552096a099424f9144efe0022b46392c
Prof. André Luiz Braga, D.Sc <andre.luiz.braga@terra.com.br>
2024-11-05 21:06:58 -0300

    dsggsg

--- PROJETO_AULA_12_EX1_LENC/ListaEncadeada/src/App.java
+++ PROJETO_AULA_12_EX1_LENC/ListaEncadeada/src/App.java
@@ -3,11 +3,38 @@ public class App {
     static Vagao fiscalVagao;
     static Vagao thomasESeusAmigos = null;
     static Vagao caixaoVelaPreta = null;
+    public static Vagao insereOrdenado(Vagao locomotiva, String nomeVagao){
+
+            // Criar o vagao
+            Vagao novoVagao = new Vagao(nomeVagao);
+
+            Vagao fiscalVagao = locomotiva;
+
+            if(locomotiva.nomeVagao.compareTo(nomeVagao) > 0 ){
+                novoVagao.portaProxVagao = locomotiva;
+                locomotiva = novoVagao;
+            }else{
+                while (fiscalVagao.portaProxVagao != caixaoVelaPreta && 
+                        fiscalVagao.portaProxVagao.nomeVagao.compareTo(nomeVagao) < 0 ) {
+                    fiscalVagao = fiscalVagao.portaProxVagao;                
+                }
+                // Assumindo que eu ACHEI o vagao com o nome MAIOR que nomeVagao
+                if (fiscalVagao.portaProxVagao == caixaoVelaPreta){
+                    fiscalVagao.portaProxVagao = novoVagao;
+                    novoVagao.portaProxVagao = caixaoVelaPreta;
+                }else{
+                    novoVagao.portaProxVagao = fiscalVagao.portaProxVagao;
+                    fiscalVagao.portaProxVagao = novoVagao;
+                }
+            }
+        return locomotiva;
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //// Criar trem ////
         Vagao v1 = new Vagao("Locomotiva");
+
         Vagao v2 = new Vagao("Restaurante");
         Vagao v3 = new Vagao("Taylor Swift");
 
@@ -16,6 +43,11 @@ public class App {
         v2.portaProxVagao = v3;
         v3.portaProxVagao = caixaoVelaPreta;
 
+
+        thomasESeusAmigos = insereOrdenado(thomasESeusAmigos, "Mercado");
+        thomasESeusAmigos = insereOrdenado(thomasESeusAmigos, "U2");
+        thomasESeusAmigos = insereOrdenado(thomasESeusAmigos, "Deposito");
+
         ///// Percorrer trem (Lista) /////
         fiscalVagao = thomasESeusAmigos;
         while ( fiscalVagao != null) {
@@ -23,6 +55,7 @@ public class App {
             fiscalVagao = fiscalVagao.portaProxVagao;
         }
         /////////////////////////////////
+        /*
         String suspeito = "Gordon";
         boolean naoAchou = true;
         fiscalVagao  = thomasESeusAmigos;
@@ -37,7 +70,7 @@ public class App {
             System.out.println("Nao achei o vagao: " + suspeito + " e o fiscal morreu!!!");
         }else{
             System.out.println("O fiscal achou o vagao ");
-        }
+        }  */
 
 
 
